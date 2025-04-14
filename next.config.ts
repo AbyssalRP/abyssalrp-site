b3c88794d0d3db31b72ced790f902d05f732d7be
AbyssalRP <abyssalrp3@gmail.com>
2025-04-14 11:08:36 -0500

    Fix: remove invalid externals config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-// next.config.ts
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.externals = {
-      ...config.externals,
-      bcrypt: 'commonjs bcrypt', // This prevents it from being bundled
-    };
-    return config;
-  },
 };
 
 export default nextConfig;

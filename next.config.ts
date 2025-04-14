d0f4240284020dc86bc8b48c55e161acc1d151d0
AbyssalRP <abyssalrp3@gmail.com>
2025-04-14 10:32:53 -0500

    Fix: mark bcrypt as external in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals = {
+      ...config.externals,
+      bcrypt: 'commonjs bcrypt', // 🛡 prevent build from bundling bcrypt
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
